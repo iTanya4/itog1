ae9591abe4b8f54f246e85567c171f150a507507
TatYana <4inenova@gmail.com>
2022-11-20 13:05:58 +0300

    add solution in Proogram

---
+++ itog1_pr/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string [] array = {"2022", "22", "Sheremetevo", "SVO", "Vnukovo", "VKO", "Domodedovo", "DME"};
+Console.WriteLine("Исходный массив:");
+
+void PrintArray(string [] arr)
+{
+    for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]}  ");
+    Console.WriteLine();
+}
+
+PrintArray(array);
+Console.WriteLine(); 
+
+void FoundArray (string [] firstarray)
+{
+     int size = firstarray.Length;
+     string [] newarray = new string [size];
+     for (int j = 0; j < size; j++)
+     {      
+        if (firstarray[j].Length <= 3)
+        {
+            newarray[j] = firstarray[j];
+            Console.Write($"{newarray[j]}  "); 
+        }
+     }
+}
+Console.WriteLine("Новый массив:");
+FoundArray(array);
+Console.WriteLine();
